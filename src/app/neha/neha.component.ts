99bddb899f6964aa9bcc43ff65c26e98d5cb364a
Neha Gholap <nehagholap98@gmail.com>
2024-02-18 09:20:28 +0530

    First Interpolation

---
+++ src/app/neha/neha.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-neha',
+  standalone: true,
+  imports: [],
+  templateUrl: './neha.component.html',
+  styleUrl: './neha.component.css'
+})
+export class NehaComponent 
+{ 
+   public fun : string = "Marvellous Infosystems";
+}
